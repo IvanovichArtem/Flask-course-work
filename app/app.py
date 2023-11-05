9ddb2bb7cfaf21f01056b6cefedb1ee5078e2b5c
IvanovichArtem <artemivanovich489@gmail.com>
2023-11-05 15:36:06 +0300

    added a shop pages and fixed other urls

--- app/app.py
+++ app/app.py
@@ -4,138 +4,215 @@ from form import LoginForm, ProfileForm, RegistrationForm
 from flask_session import Session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "SECRET_KEY very long"
-app.config['SESSION_TYPE'] = 'filesystem'
-db = Database(db_name="flask_museum", user="admin", password="root") 
+app.config["SECRET_KEY"] = "SECRET_KEY very long"
+app.config["SESSION_TYPE"] = "filesystem"
+db = Database(db_name="flask_museum", user="admin", password="root")
 Session(app)
 
-#MAIN CONTENT
+
+# MAIN CONTENT
 @app.route("/")
 def index():
     context = {"title": "Главная"}
     return render_template("catalog/index.html", context=context)
 
+
 @app.route("/about")
 def about():
-    context = {
-        'title': "О нас"
-    }
+    context = {"title": "О нас"}
     return render_template("catalog/about.html", context=context)
+
 
 @app.route("/contacts")
 def contacts():
-    context = {
-        'title': "Контакты"
-    }
+    context = {"title": "Контакты"}
     return render_template("catalog/contacts.html", context=context)
+
 
 @app.route("/exhibitions")
 def exhibitions():
     context = {
-        'title': 'Выставки',
-        'exhibitions': db.filter(table_name='catalog_exhibition', type_id=1)
+        "title": "Выставки",
+        "exhibitions": db.filter(table_name="catalog_exhibition", type_id=1),
     }
     return render_template("catalog/exhibition.html", context=context)
+
 
 @app.route("/events")
 def events():
     context = {
-        'title': 'Выставки',
-        'events': db.filter(table_name='catalog_exhibition', type_id=2)
+        "title": "Выставки",
+        "events": db.filter(table_name="catalog_exhibition", type_id=2),
     }
     return render_template("catalog/events.html", context=context)
 
-#USER METHODS
-@app.route("/user/login", methods=['GET', 'POST'])
+
+# USER METHODS
+@app.route("/user/login", methods=["GET", "POST"])
 def login():
     form = LoginForm(request.form)
-    if request.method == 'POST':
+    if request.method == "POST":
         if not form.validate():
-            print('Invalid login form')
+            print("Invalid login form")
         else:
-            if db.exists('user_user', username=form.data['username'], password=form.data['password']):
-                session['logged_in'] = True
-                session['username'] = form.data['username']
-                session['user_id'] = db.get_user_id('user_user', username=form.data['username'])
-                return redirect(url_for('index'))
+            if db.exists(
+                "user_user",
+                username=form.data["username"],
+                password=form.data["password"],
+            ):
+                session["logged_in"] = True
+                session["username"] = form.data["username"]
+                session["user_id"] = db.get_user_id(
+                    "user_user", username=form.data["username"]
+                )
+                return redirect(url_for("index"))
             else:
-                return render_template("user/login.html", form=form, error='Неправильный логин или пароль')
+                return render_template(
+                    "user/login.html", form=form, error="Неправильный логин или пароль"
+                )
     else:
         return render_template("user/login.html", form=form)
+
 
 @app.route("/user/registration", methods=["POST", "GET"])
 def registration():
     form = RegistrationForm(request.form)
-    if request.method == 'POST':
+    if request.method == "POST":
         if not form.validate():
-            return render_template("user/registration.html", form=form, errors=[elem[0] for elem in form.errors.values()])
+            return render_template(
+                "user/registration.html",
+                form=form,
+                errors=[elem[0] for elem in form.errors.values()],
+            )
         else:
-            if db.exists('user_user', username=form.data['username']):
-                return render_template("user/registration.html", form=form, error='Существет пользователь с таким логином')
+            if db.exists("user_user", username=form.data["username"]):
+                return render_template(
+                    "user/registration.html",
+                    form=form,
+                    error="Существет пользователь с таким логином",
+                )
             else:
-                db.create(table_name='user_user', username =form.data['username'], last_name=form.data['last_name'],
-                        password=form.data['password1'], first_name=form.data['first_name'],
-                        email=form.data['email'])
-                session['logged_in'] = True
-                session['username'] = form.data['username']
-                session['user_id'] = db.get_user_id('user_user', username=form.data['username'])
-                return redirect(url_for('index'))
+                db.create(
+                    table_name="user_user",
+                    username=form.data["username"],
+                    last_name=form.data["last_name"],
+                    password=form.data["password1"],
+                    first_name=form.data["first_name"],
+                    email=form.data["email"],
+                )
+                session["logged_in"] = True
+                session["username"] = form.data["username"]
+                session["user_id"] = db.get_user_id(
+                    "user_user", username=form.data["username"]
+                )
+                return redirect(url_for("index"))
     else:
         return render_template("user/registration.html", form=form)
 
-@app.route("/user/profile", methods=['GET', 'POST'])
+
+@app.route("/user/profile", methods=["GET", "POST"])
 def profile():
     form = ProfileForm(request.form)
-    user = db.filter(table_name='user_user', username=session['username'])[0]
-    baskets = db.query(f"""SELECT cb.id, cb.quantity, ce.name,
+    user = db.filter(table_name="user_user", username=session["username"])[0]
+    baskets = db.query(
+        f"""SELECT cb.id, cb.quantity, ce.name,
                         ce.description, ce.price
                          FROM catalog_basket cb 
                        JOIN catalog_exhibition ce
                         ON ce.id=cb.exhibition_id
-                        WHERE cb.user_id = {user['id']}""")
-    if request.method == 'POST':
-        uppload_file = request.files['img']
-        if uppload_file.filename != '':
-            file_path = 'app/static/media/users_images/' + uppload_file.filename
+                        WHERE cb.user_id = {user['id']}"""
+    )
+    total_count = sum([basket["quantity"] for basket in baskets])
+    total_price = sum([basket["quantity"] * basket["price"] for basket in baskets])
+    if request.method == "POST":
+        uppload_file = request.files["img"]
+        if uppload_file.filename != "":
+            file_path = "app/static/media/users_images/" + uppload_file.filename
             uppload_file.save(file_path)
             changed_data = {
-                'first_name': form.data['first_name'],
-                'last_name': form.data['last_name'],
-                'img': 'users_images/' + uppload_file.filename
+                "first_name": form.data["first_name"],
+                "last_name": form.data["last_name"],
+                "img": "users_images/" + uppload_file.filename,
             }
         else:
             changed_data = {
-                'first_name': form.data['first_name'],
-                'last_name': form.data['last_name'],
+                "first_name": form.data["first_name"],
+                "last_name": form.data["last_name"],
             }
-        
-        db.update(table_name='user_user', key='id', key_value=user['id'], **changed_data)
-        return redirect('profile') 
+
+        db.update(
+            table_name="user_user", key="id", key_value=user["id"], **changed_data
+        )
+        return redirect("profile")
     else:
-        return render_template("user/profile.html", form=form,
-                            title='Профиль', user=user, baskets=baskets, )  
+        return render_template(
+            "user/profile.html",
+            form=form,
+            title="Профиль",
+            user=user,
+            baskets=baskets,
+            total_count=total_count,
+            total_price=total_price,
+        )
 
-@app.route('/user/logout')
+
+@app.route("/user/logout")
 def logout():
-    print(session['username'])
-    session['username'] = None
-    session['logged_in'] = False
-    session['user_id'] = None
-    return redirect(url_for('index'))
+    print(session["username"])
+    session["username"] = None
+    session["logged_in"] = False
+    session["user_id"] = None
+    return redirect(url_for("index"))
 
-@app.route('/user/basket_add/<int:exhibit_id>')
+
+@app.route("/user/basket_add/<int:exhibit_id>")
 def basket_add(exhibit_id):
-    if session['logged_in']:
-        exhibition = db.filter(table_name='catalog_exhibition', id=exhibit_id)[0]
-        basket = db.filter(table_name='catalog_basket', user_id=session['user_id'], exhibition_id=exhibition['id'])
+    if session["logged_in"]:
+        exhibition = db.filter(table_name="catalog_exhibition", id=exhibit_id)[0]
+        basket = db.filter(
+            table_name="catalog_basket",
+            user_id=session["user_id"],
+            exhibition_id=exhibition["id"],
+        )
         if basket == []:
-            db.create(table_name='catalog_basket', quantity=1, exhibition_id=exhibition['id'],
-                      user_id=session['user_id'])
+            db.create(
+                table_name="catalog_basket",
+                quantity=1,
+                exhibition_id=exhibition["id"],
+                user_id=session["user_id"],
+            )
         else:
-            db.update(table_name='catalog_basket', key='id', key_value=basket[0]['id'], quantity = basket[0]['quantity']+1)
+            db.update(
+                table_name="catalog_basket",
+                key="id",
+                key_value=basket[0]["id"],
+                quantity=basket[0]["quantity"] + 1,
+            )
 
         return redirect(request.referrer)
     else:
-        return redirect(url_for('login'))
-#TODO: make a methods to add a basket and baskets
+        return redirect(url_for("login"))
 
+
+@app.route("/user/basket_delete/<int:basket_id>")
+def delete_basket(basket_id):
+    db.delete(table_name="catalog_basket", condition=f"id = {basket_id}")
+    return redirect(request.referrer)
+
+
+@app.route("/shop", defaults={"category_id": None})
+@app.route("/shop/<int:category_id>")
+def shop(category_id):
+    categories = db.all(table_name="shop_producttype")
+    if category_id:
+        products = db.filter(table_name="shop_product", category_id=category_id)
+    else:
+        products = db.all(table_name="shop_product")
+    return render_template(
+        "shop/shop.html", title="Магазин", categories=categories, products=products
+    )
+
+
+@app.route("/shop/about")
+def shop_about():
+    return render_template("shop/about.html", title="О магазине")
--- app/db/db.py
+++ app/db/db.py
@@ -1,9 +1,13 @@
 from psycopg2 import connect
 from psycopg2.extras import RealDictCursor
+from typing_extensions import List, OrderedDict
+
 
 class Database:
     def __init__(self, db_name, user, password, host="localhost", port="5432"):
-        self.conn = connect(dbname=db_name, host=host, user=user, password=password, port=port)
+        self.conn = connect(
+            dbname=db_name, host=host, user=user, password=password, port=port
+        )
         self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
 
     def __del__(self):
@@ -11,21 +15,19 @@ class Database:
         self.conn.commit()
         self.conn.close()
 
-    def all(self, **kwargs):
-        self.cursor.execute(f"SELECT * FROM {kwargs['table_name']}")
+    def all(self, table_name) -> List[OrderedDict]:
+        self.cursor.execute(f"SELECT * FROM {table_name}")
         fetch = self.cursor.fetchall()
-        return fetch 
-    
+        return fetch
+
     def last_login(self, **kwargs):
-        self.cursor.execute(f"UPDATE {kwargs['table_name']} SET last_login=current_timestamp where username = '{kwargs['username']}'")
+        self.cursor.execute(
+            f"UPDATE {kwargs['table_name']} SET last_login=current_timestamp where username = '{kwargs['username']}'"
+        )
         self.conn.commit()
 
-    def filter(self, **kwargs):
+    def filter(self, table_name: str, **kwargs) -> List[OrderedDict]:
         """sumary_line
-        
-        Keyword arguments:
-        table_name - required -> Str
-        other_conditions:
             id=1 \n
             id__gt=4 ---> id > 4 \n
             id__gte=4 ---> id >= 4 \n
@@ -33,14 +35,12 @@ class Database:
             id__lte=4 ---> id <= 4 \n
             id__in=(1,2,3) ---> id IN (1, 2, 3) \n
 
-        Return: fetch
+        Return: [OrderedDict]
         """
-        table_name = kwargs['table_name']
-        del kwargs['table_name']
         query_expression = []
         for key, value in kwargs.items():
             if "__" in key:
-                prefix, postfix = key.split('__')
+                prefix, postfix = key.split("__")
             else:
                 if type(value) is str:
                     query_expression.append(f"{key} = '{value}'")
@@ -59,20 +59,29 @@ class Database:
             elif postfix == "in":
                 query_expression.append(f"{prefix} IN {value}")
 
-        self.cursor.execute(f"SELECT * FROM {table_name} WHERE {' AND '.join(query_expression)}")
+        self.cursor.execute(
+            f"SELECT * FROM {table_name} WHERE {' AND '.join(query_expression)}"
+        )
         fetch = self.cursor.fetchall()
 
         return fetch
-    
-    def create(self, table_name, **kwargs):
+
+    def create(self, table_name: str, **kwargs):
         """INSERT INTO user_user(username,last_name,password,first_name,email)
-           VALUES('username','last_name','password','first','email');"""
+        VALUES('username','last_name','password','first','email');"""
         keys = [str(i) for i in kwargs.keys()]
         values = [f"'{i}'" for i in kwargs.values()]
-        self.cursor.execute(f"INSERT INTO {table_name}({','.join(keys)}) VALUES({','.join(values)})")
+        self.cursor.execute(
+            f"INSERT INTO {table_name}({','.join(keys)}) VALUES({','.join(values)})"
+        )
         self.conn.commit()
-        
-    def exists(self, table_name, **kwargs):
+
+    def delete(self, table_name: str, condition: str):
+        query = f"DELETE FROM {table_name} WHERE {condition}"
+        self.cursor.execute(query)
+        self.conn.commit()
+
+    def exists(self, table_name: str, **kwargs):
         self.cursor.execute(f"SELECT * FROM {table_name}")
         fetch = self.cursor.fetchall()
         for row in fetch:
@@ -81,10 +90,10 @@ class Database:
                 return True
         return False
 
-    def update(self, table_name, key, key_value, **kwargs):
-        """UPDATE weather SET 
-        (first_name, last_name, img) = (first_name, last_name, img)
-  WHERE id=id;"""
+    def update(self, table_name: str, key, key_value, **kwargs):
+        """UPDATE weather SET
+              (first_name, last_name, img) = (first_name, last_name, img)
+        WHERE id=id;"""
         keys = [str(i) for i in kwargs.keys()]
         values = [f"'{i}'" if (type(i) is str) else f"{i}" for i in kwargs.values()]
         if len(kwargs) == 1:
@@ -93,19 +102,20 @@ class Database:
             query = f"UPDATE {table_name} SET ({','.join(keys)}) = ({','.join(values)}) WHERE {key}={key_value};"
         self.cursor.execute(query)
         self.conn.commit()
-    
-    def get_user_id(self, table_name, **kwargs):
+
+    def get_user_id(self, table_name: str, **kwargs):
         key = list(kwargs.keys())[0]
         value = kwargs.get(key)
         query = f"SELECT id from {table_name} WHERE {key} = '{value}'"
         self.cursor.execute(query)
         fetch = self.cursor.fetchone()
-        return fetch['id']
-    
-    def query(self, query):
+        return fetch["id"]
+
+    def query(self, query: str) -> List[OrderedDict]:
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
+
 if __name__ == "__main__":
     db = Database(db_name="flask_museum", user="admin", password="root")
-    db.get_user_id(table_name='user_user', username='Lagrush')
+    db.get_user_id(table_name="user_user", username="Lagrush")
